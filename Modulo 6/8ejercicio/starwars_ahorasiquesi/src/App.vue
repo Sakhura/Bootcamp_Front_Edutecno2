5d0d7a786df6b56cf73247f93b6c88d9ba41c4b5
Sakhura <sabina.romero@gmail.com>
2024-11-15 21:03:28 -0300

    imagen ok

--- Modulo 6/8ejercicio/starwars_ahorasiquesi/src/App.vue
+++ Modulo 6/8ejercicio/starwars_ahorasiquesi/src/App.vue
@@ -1,53 +1,60 @@
 <template>
   <div class="general">
-    <ul class="nav" style="background-color:yellow;">
-            <li class="nav-item">                
-                <router-link class="nav-link" to="/">Inicio | </router-link>                 
-            </li>
-            <li class="nav-item">              
-                <router-link class="nav-link" to="/personajes">Personajes |</router-link>
-            </li>
-            <li class="nav-item">                
-                <router-link class="nav-link" to="/contacto">Contacto</router-link>
-            </li>         
-        </ul>
- 
+         <ul class="nav" style="background-color: yellow;">
+      <li class="nav-item">
+        <router-link to="/home" class="nav-link">Inicio</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/personajes" class="nav-link">Personajes</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/contacto" class="nav-link">Contacto</router-link>
+      </li>
+            </ul>
+     <img :src="imageUrl" alt="Star Wars" />
     <router-view/>
   </div>
- 
+   
 </template>
 
+<script>
+export default {
+      data() {
+  return {
+    imageUrl: 'https://brandemia.org/contenido/subidas/2021/05/portada-starwars-imagenes-brandemia-blog-1000x670.jpg'
+  };
+}
+}
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+font-family: Avenir, Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: center;
+color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
+img {
+max-width: 100%;
+height: auto;
+}
+#nav{
+padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+font-weight: bold;
+color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#nav a.router-link-exact-active{
+color: #42b983;
 }
 
 .general{
-  background-color: black;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-
+background-color: black;
+width: 100%;
+height: 100%;
+position: absolute;
 }
-
-
-
-
 </style>
