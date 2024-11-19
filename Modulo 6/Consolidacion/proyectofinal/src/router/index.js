b207c82650a0b8fee39131852ea22e6dbcac4418
Sakhura <sabina.romero@gmail.com>
2024-11-19 19:30:32 -0300

    base proyecto final

---
+++ Modulo 6/Consolidacion/proyectofinal/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Opiniones from '../views/Opiniones.vue'
+import Administracion from '../views/Administracion.vue'
+import NotFound from '../views/NotFound.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+{
+    path: '/',
+    name: 'Home',
+    component: Home
+},
+{
+    path: '/opiniones/:id',
+    name: 'Opiniones',
+    component: Opiniones,
+
+},
+{
+    path: '/administracion',
+    name: 'Administracion',
+    component: Administracion
+},
+
+{
+    path: '/*',
+    name: 'NotFound',
+    component: NotFound
+}
+]
+
+const router = new VueRouter({
+mode: 'history',
+base: process.env.BASE_URL,
+routes
+})
+
+export default router
