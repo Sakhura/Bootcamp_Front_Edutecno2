332af4119a8d35c31da44add34d110db5830e9b5
Sakhura <sabina.romero@gmail.com>
2024-11-19 21:18:56 -0300

    falta diseño y funcionalidad

--- Modulo 6/Consolidacion/proyectofinal/src/App.vue
+++ Modulo 6/Consolidacion/proyectofinal/src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <div id="app">
-    <NavBar :navItems="navBarItems" />
-    <router-view/>
-  </div>
+  <HomePage msg="HomePage"/>
+  <navBar msg="NavBar"/>
 </template>
-
+ 
 <script>
-import navBar from '@/components/navBar.vue'
+import navBar from './components/navBar.vue'
+import HomePage from './views/HomePage.vue';
 
+ 
 export default {
   name: 'App',
   components: {
-    navBar
+    
+    navBar,
+    HomePage
   }
 }
-
 </script>
-
-
+ 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- Modulo 6/Consolidacion/proyectofinal/src/views/HomePage.vue
+++ Modulo 6/Consolidacion/proyectofinal/src/views/HomePage.vue
@@ -43,7 +43,7 @@
   // import HelloWorld from '@/components/HelloWorld.vue'
   
   export default {
-    name: 'Home',
+    name: 'HomePage',
     components: {
       // HelloWorld
   
